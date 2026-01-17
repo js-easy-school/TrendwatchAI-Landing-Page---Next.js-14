f4ac3232581daed7ae55f4acd35e9076214844be
js-easy-school <supergeroy11@gmail.com>
2026-01-17 19:10:12 +0300

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === 'production' ? "/TrendwatchAI-Landing-Page---Next.js-14" : "",
+  assetPrefix: process.env.NODE_ENV === 'production' ? "/TrendwatchAI-Landing-Page---Next.js-14/" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
